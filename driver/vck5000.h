daeab2ebf48c575dd49574a87a31a68b1204355c
eddierichter-amd <eddie.richter@amd.com>
2024-04-04 12:33:29 -0600

    Driver timeout (#10)
    * Adding a timeout to the driver
    
    * Formatting

--- driver/vck5000.h
+++ driver/vck5000.h
@@ -10,6 +10,12 @@
 #define VCK5000_AIE_MEM_RANGE 0x20000000
 
 /**
+ * When the kernel will timeout after sending
+ * a command
+ */
+#define KERNEL_CMD_TIMEOUT_VAL 10000
+
+/**
  * vck5000_dev_init - Initialize a VCK5000 device.
  */
 void vck5000_dev_init(struct amdair_device *air_dev);
